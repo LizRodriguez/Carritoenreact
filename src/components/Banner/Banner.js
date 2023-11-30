a0051d386c43e3d5513ee63f357f08253c550cda
LizRodriguez <20040524@itdurango.edu.mx>
2023-11-30 00:20:56 -0600

    primer commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+       <div className="banner-container">
+        <h1>Tienda de Ropa</h1>
+        <p>Vistete a la Moda</p>
+        </div>
+        </div>
+  )
+}
+export default Banner;
